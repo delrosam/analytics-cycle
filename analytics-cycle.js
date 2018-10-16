5d8a2b374705cc1e83dc3b170002421c46f285e1
Marnel Mangrubang <delrosam@gmail.com>
2018-10-16 09:59:56 -0700

    my first commit

---
+++ analytics-cycle.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         AS Google Analytics Real Time Data Cycle
+// @namespace    https://analytics.google.com/
+// @version      0.1
+// @description  This script with cycle through the menu options on the left to rotate through the real time data
+// @author       Marnel Mangrubang and Steven Turner
+// @match        https://analytics.google.com/*
+// @grant        none
+// @require      https://code.jquery.com/jquery-3.3.1.min.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    
+})();
